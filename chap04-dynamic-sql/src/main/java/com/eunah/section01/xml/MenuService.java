4c1d67481b7b01c1bbed024a2ffdad8a670ec4d5
EunahPae <eunahp86@gmail.com>
2024-05-31 16:56:07 +0900

    0531

--- chap04-dynamic-sql/src/main/java/com/eunah/section01/xml/MenuService.java
+++ chap04-dynamic-sql/src/main/java/com/eunah/section01/xml/MenuService.java
@@ -6,7 +6,6 @@ import org.apache.ibatis.session.SqlSession;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import static com.eunah.common.Template.getSqlSession;
@@ -14,7 +13,6 @@ import static com.eunah.common.Template.getSqlSession;
 public class MenuService {
 
     private DynamicSqlMapper mapper;
-
 
     public void selectMenuByPrice(int price) {
         SqlSession sqlSession = getSqlSession();
@@ -24,16 +22,14 @@ public class MenuService {
         map.put("price", price);
         List<MenuDTO> menuList = mapper.selectMenuByPrice(map);
 
-        if (menuList != null && menuList.size()>0) {
+        if (menuList != null && menuList.size() > 0) {
             for (MenuDTO menu : menuList) {
                 System.out.println(menu);
             }
-        }else{
+        } else {
             System.out.println("검색 결과가 없습니다. ");
         }
         sqlSession.close();
-
-
     }
 
     public void searchMenu(SearchCriteria searchCriteria) {
@@ -42,14 +38,13 @@ public class MenuService {
 
         List<MenuDTO> menuList = mapper.searchMenu(searchCriteria);
 
-        if (menuList != null && menuList.size()>0) {
+        if (menuList != null && menuList.size() > 0) {
             for (MenuDTO menu : menuList) {
                 System.out.println(menu);
             }
-        }else{
+        } else {
             System.out.println("검색 결과가 없습니다. ");
         }
         sqlSession.close();
-
     }
 }
